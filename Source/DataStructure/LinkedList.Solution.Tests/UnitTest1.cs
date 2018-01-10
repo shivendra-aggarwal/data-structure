2640f7e887226b6303a7694af584a636c4b1d4e5
Shivendra Aggarwal <shivendra.aggarwal@outlook.com>
2018-01-10 11:56:05 +0530

    added support files for data structure

---
+++ Source/DataStructure/LinkedList.Solution.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LinkedList.Solution.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Source/DataStructure/LinkedList.Solution/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LinkedList.Solution
+{
+    public class Class1
+    {
+    }
+}
