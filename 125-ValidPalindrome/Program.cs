fd497ac3cc83db5e2b8abb640a21918b885a8f61
Shivendra <shivendra.aggarwal@gmail.com>
2025-02-18 09:41:45 +0530

    added solution for Leetcode 125 problem

---
+++ 125-ValidPalindrome/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+
+bool IsPalindrome(string s)
+{
+    int l = 0; int r = s.Length - 1;
+
+    while (l < r)
+    {
+        if (!Char.IsLetterOrDigit(s[l]))
+        {
+            l++;
+            continue;
+        }
+
+        if (!Char.IsLetterOrDigit(s[r]))
+        {
+            r--;
+            continue;
+        }
+
+
+        if (Char.ToLower(s[l]) != Char.ToLower(s[r]))
+        {
+            return false;
+        }
+
+        l++;
+        r--;
+
+    }
+
+    return true;
+}
+
+Console.WriteLine(IsPalindrome("A man, a plan, a canal: Panama"));
+
+Console.WriteLine(IsPalindrome("race a car"));
+
+Console.WriteLine(IsPalindrome(" "));
