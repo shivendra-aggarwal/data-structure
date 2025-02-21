5f0999fa4ced91fbb804104ede204d60e2ab9362
Shivendra <shivendra.aggarwal@gmail.com>
2025-02-21 22:27:11 +0530

    added leetcode solution of problem no 191 and 338

---
+++ 191-Numberof1Bits/Program.cs
@@ -0,0 +1,14 @@
+﻿int HammingWeight(int n)
+{
+    int count = 0;
+
+    while (n > 0)
+    {
+        count++;
+        n = n & (n - 1);
+    }
+    return count;
+}
+
+
+Console.WriteLine(HammingWeight(2147483645));
---
+++ 338-CountingBits/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] CountBits(int n)
+{
+    int[] result = new int[n + 1];
+    int offset = 1;
+
+    for (int i = 1; i < n + 1; i++)
+    {
+        if (offset * 2 == i)
+            offset = i;
+        result[i] = 1 + result[i - offset];
+    }
+
+    return result;
+}
+
+Console.WriteLine(string.Join(" ", CountBits(2)));
+
+
